e8611cf222c4b15081a246e99ad3e60d2279ca64
ivanovit <i.ivanov.it@gmail.com>
2015-01-08 10:31:45 +0200

    Implement Github search api for repositories
    The purpose of this commit is to extend GitHub API rate to 10 requests per minute by using GitHub search api(the main benefit is that the search api has it's own rate limit). By this way we also will have the advantage to query the organization repositories instead of getting all repositories for organization and filtering them on client.

--- Github.Api/Core/GitHubApi.cs
+++ Github.Api/Core/GitHubApi.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Github.Api.Extensions;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Github.Api.Core
